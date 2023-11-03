4b3fd16276207801a2f0aae64449879081ab67a7
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2023-11-03 19:56:12 +0900

    Correct variables names @ alternative code version

--- 100-days-of-code/day-3-treasure-island/treasure-island.py
+++ 100-days-of-code/day-3-treasure-island/treasure-island.py
@@ -56,11 +56,11 @@ if crossroad == "left":
   middle_lake = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
   if middle_lake == "wait":
     doors = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
-    if choice3 == "red":
+    if doors == "red":
       print("It was the gate for the Hades! You was burned by fire! Game Over!")
-    elif choice3 == "blue":
+    elif doors == "blue":
       print("You enter a room of beasts. Game Over.")
-    elif choice3 == "yellow":
+    elif doors == "yellow":
       print("You found the treasure! You Win!")
     else:
       print("You chose a door that doesn't exist. Game Over.")
