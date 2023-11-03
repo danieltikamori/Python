bb907a56c4cdc0b087056ed4e36ccfcf90658903
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2023-11-03 18:44:29 +0900

    Comment with how a leap year is determined.

--- 100-days-of-code/day-3-treasure-island/check-if-a-year-is-leap-year-or-not.py
+++ 100-days-of-code/day-3-treasure-island/check-if-a-year-is-leap-year-or-not.py
@@ -1,4 +1,15 @@
 # Check if a year is leap year or not
+
+"""
+This is how you work out whether if a particular year is a leap year:
+
+- on every year that is divisible by 4 with no remainder
+
+- except every year that is evenly divisible by 100 with no remainder
+
+- unless the year is also divisible by 400 with no remainder
+"""
+
 year = int(input("Which year do you want to check? Type here: "))
 
 if year % 4 == 0:
