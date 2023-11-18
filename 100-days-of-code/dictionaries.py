77f88ac68576d95b63adb10c8bd5eeaddd2dcd60
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2023-11-18 19:55:54 +0900

    Update with a project using dictionaries

--- 100-days-of-code/dictionaries.py
+++ 100-days-of-code/dictionaries.py
@@ -165,3 +165,30 @@ person = {
 if "name" in person:
     print("The key 'name' exists in the dictionary.")
     # Output: The key 'name' exists in the dictionary.
+
+# Grading program
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+# 1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# 2: Covert scores into grades.
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
