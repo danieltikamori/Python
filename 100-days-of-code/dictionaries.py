b35b9cd8c1f3a8ccddce7420717fbecf01a43037
Daniel Tikamori <57825640+danieltikamori@users.noreply.github.com>
2023-11-18 22:02:41 +0900

    Update with an exercise on dictionary in list

--- 100-days-of-code/dictionaries.py
+++ 100-days-of-code/dictionaries.py
@@ -227,3 +227,47 @@ travel_log = [
   "total_visits": 5,
 },
 ]
+
+# Exercise: Dictionary in List
+
+country = input() # Add country name
+visits = int(input()) # Number of visits
+list_of_cities = eval(input()) # create list from formatted string
+
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+
+# Task: Write the function that will allow new countries
+# to be added to the travel_log. 
+def add_new_country(name_of_country, times_visited, cities_visited):
+    """
+  Adds a new country to the travel log with the given information.
+
+  Parameters:
+    name_of_country (str): The name of the country.
+    times_visited (int): The number of times visited.
+    cities_visited (list): A list of cities visited in the country.
+  Returns:
+    None
+    """
+
+    new_country = {}
+    new_country["country"] = name_of_country
+    new_country["visits"] = times_visited
+    new_country["cities"] =  cities_visited
+    travel_log.append(new_country)
+
+# Call the function with the appropriate arguments
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
